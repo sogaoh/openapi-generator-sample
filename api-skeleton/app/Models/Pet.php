0fae5e6fe2644b08e1e94ba6a3e688d48db1d084
sogaoh <sogaoh@ant-in-giant.work>
2020-10-04 18:06:29 +0900

    add generated codes (php-laravel)

---
+++ api-skeleton/app/Models/Pet.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pet
+ */
+namespace app\Models;
+
+/**
+ * Pet
+ */
+class Pet {
+
+    /** @var int $id */
+    private $id;
+
+    /** @var string $name */
+    private $name;
+
+    /** @var string $tag */
+    private $tag;
+
+}
